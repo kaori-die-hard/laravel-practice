a7c4a04861dc10ea9c5baa09e2315bfa1b7005b0
Kaori Nakamura <kaorinakamura@KaorinoMacBook-Air.local>
2025-09-11 15:17:42 +0900

    ログイン機能

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,28 +12,28 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->id()->comment('ユーザID');
+            $table->string('name')->comment('名前');
+            $table->string('email')->unique()->comment('メールアドレス');
+            $table->timestamp('email_verified_at')->nullable()->comment('メール検証日時');
+            $table->string('password')->comment('パスワード');
+            $table->rememberToken()->comment('トークン');
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->string('email')->primary()->comment('メールアドレス');
+            $table->string('token')->comment('トークン');
+            $table->timestamp('created_at')->nullable()->comment('作成日時');
         });
 
         Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->string('id')->primary()->comment('ID');
+            $table->foreignId('user_id')->nullable()->index()->comment('ユーザID');
+            $table->string('ip_address', 45)->nullable()->comment('IPアドレス');
+            $table->text('user_agent')->nullable()->comment('ユーザーエージェント');
+            $table->longText('payload')->comment('ペイロード');
+            $table->integer('last_activity')->index()->comment('最終アクティビティ');
         });
     }
 
